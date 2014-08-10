23ccd385c16ddc96dcbbf175724633fad830b742
Spencer MacDonald <spencer.macdonald@gmail.com>
2014-08-10 15:57:31 +0100

    Recompiled JS with Latest Dependencies

--- compile_libphonenumber.rb
+++ compile_libphonenumber.rb
@@ -11,17 +11,17 @@ FileUtils.rm_rf(@build_directory_path) if File.exists?(@build_directory_path)
 system("svn checkout http://libphonenumber.googlecode.com/svn/trunk/ #{@build_directory_path}/libphonenumber")
 
 # Checkout closure-library
-system("svn checkout http://closure-library.googlecode.com/svn/trunk/ #{@build_directory_path}/closure-library")
+system("git clone git://github.com/google/closure-library.git #{@build_directory_path}/closure-library")
 
 # Download closure-compiler
-system("curl -f -L http://closure-compiler.googlecode.com/files/compiler-latest.zip > #{@build_directory_path}/compiler-latest.zip")
+system("curl -f -L http://dl.google.com/closure-compiler/compiler-latest.zip > #{@build_directory_path}/compiler-latest.zip")
 system("unzip -o -d #{@build_directory_path} #{@build_directory_path}/compiler-latest.zip compiler.jar")
 
 # Change to Build Directory
 Dir.chdir(@build_directory_path)
 
 # Compile libphonenumber
-cmd = "closure-library/closure/bin/build/closurebuilder.py " \
+cmd = "python closure-library/closure/bin/build/closurebuilder.py " \
       "--root=closure-library "\
       "--namespace=\"i18n.phonenumbers.PhoneNumberUtil\" " \
       "--namespace=\"i18n.phonenumbers.AsYouTypeFormatter\" " \
